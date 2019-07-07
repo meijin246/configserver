4a0ee263c059c39a3003fb13e3cd30c9ec74ff44
liuyang <1269979614@qq.com>
2019-07-07 22:24:16 +0800

    eureka Dalston.SR1 completed

--- src/main/java/com/person/demo/DemoApplication.java
+++ src/main/java/com/person/demo/DemoApplication.java
@@ -1,10 +1,10 @@
 package com.person.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
